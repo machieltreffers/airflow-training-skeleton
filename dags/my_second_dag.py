bf6808230ccee24a949a14c1d2077c502dda1194
Machiel Treffers <mtreffers@bol.com>
2020-01-28 14:31:45 +0100

    Initial my_second_dag.py

---
+++ dags/my_second_dag.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonhOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'mtreffers',
+    'start_date': datetime(2020, 1, 27),
+}
+
+dag = DAG(
+    dag_id='my_second_dag',
+    default_args=args,
+    #    schedule_interval='45 13 * * 1,3,5',
+    schedule_interval=timedelta(minutes=150),
+    #    dagrun_timeout=timedelta(minutes=60),
+)
+
+
+def _print_exec_date(**context):
+    print(context["execution_date"])
+
+
+print_execution_date = PythonOperator(
+    task_id="print_execution_date",
+    python_callable=_print_exec_date,
+    provide_context=True,
+    dag=dag,
+)
+
+wait_5 = BashOperator(
+    task_id='wait_5',
+    bash_command='sleep 5',
+    dag=dag,
+)
+
+wait_1 = BashOperator(
+    task_id='wait_1',
+    bash_command='sleep 1',
+    dag=dag,
+)
+
+wait_10 = BashOperator(
+    task_id='wait_10',
+    bash_command='sleep 10',
+    dag=dag,
+)
+
+
+the_end = DummyOperator(
+    task_id='the_end',
+    dag=dag,
+)
+
+
+print_execution_date >> wait_5 >> the_end
+print_execution_date >> wait_1 >> the_end
+print_execution_date >> wait_10 >> the_end
