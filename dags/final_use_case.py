4c4a643a36f1a653f109d82f021b64596501de0f
Machiel Treffers <mtreffers@bol.com>
2020-01-29 14:25:56 +0100

    http to gcs

--- dags/final_use_case.py
+++ dags/final_use_case.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 import airflow
 from airflow.models import DAG
-from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+#from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
 from operators.http_to_gcs_operator import HttpToGcsOperator
 
@@ -16,3 +16,13 @@ dag = DAG(
     default_args=args,
     schedule_interval='@daily',
 )
+
+http_to_gcs = HttpToGcsOperator(
+    task_id='http_to_gcs',
+    http_conn_id='http_final_use_case',
+    endpoint='history?start_at=2018-01-01&end_at=2018-01-04&symbols=EUR&base=GBP',
+    gcs_bucket='final_use_case',
+    dag=dag,
+)
+
+http_to_gcs
